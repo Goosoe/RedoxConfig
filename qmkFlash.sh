9a01b026ebc8e53a0aa416ba5fdc60ee2b01f03b
Goosoe <miguel.al.lourenco@gmail.com>
2025-11-10 18:44:19 +0000

    Update qmkFlash

--- qmkFlash.sh
+++ qmkFlash.sh
@@ -1,5 +1,10 @@
 path="$HOME/qmk_firmware/keyboards/redox/keymaps/RedoxConfig"
 cd $path
-cp "$HOME/Downloads/goosoe.json" $path
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied, copying from Downloads"
+    cp "$HOME/Downloads/goosoe.json" $path
+elif [ $# -eq "local" ]; then
+    echo "Building with local .json"
+fi
 qmk json2c goosoe.json -o keymap.c
 qmk flash -kb redox/rev1/base -km RedoxConfig
